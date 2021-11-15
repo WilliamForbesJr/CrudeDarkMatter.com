926c17519a1ad29cf63a6f370d61887006916ddc
Will Forbes <42383119+WilliamForbesJr@users.noreply.github.com>
2021-11-14 19:55:17 -0500

    Add responsive classes and state for nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,39 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
 import logoImg from '../images/CoverArt_512.png';
 
-const Header = ({ siteTitle }) => (
-  <header className="header">
-    <div className="logo flex flex__align--center">
-      <img src={logoImg} style={{ maxHeight: '40px' }} />
-      <h1 className="logo">Crude Dark Matter</h1>
-    </div>
-    <nav className="header__nav animate-underline animate-underline--left">
-      <Link to="/">Home </Link>
-      <a href="#">Press Kit</a>
-      <Link to="/about">About </Link>
-      <a href="#">Contact</a>
-      <a href="#">Blog</a>
-    </nav>
-  </header>
-);
+const Header = ({ siteTitle }) => {
+  // Mobile Menu toggle state
+  const [menuOpen, setMenuOpen] = useState(false);
+  
+  return (
+    <header className="header">
+      <div className="logo flex flex__align--center">
+        <img src={logoImg} style={{ maxHeight: '40px' }} />
+        <h1 className="logo">Crude Dark Matter</h1>
+      </div>
+      <nav className="nav">
+        <ul className={`header__nav ${menuOpen ? 'header__nav--active' : ''}`}>
+          <Link className="header__nav-item" to="/">Home </Link>
+          <a className="header__nav-item" href="#">Press Kit</a>
+          <Link className="header__nav-item" to="/about">About </Link>
+          <a className="header__nav-item" href="#">Contact</a>
+          <a className="header__nav-item" href="#">Blog</a>
+        </ul>
+        <div
+          className={`header__hamburger ${menuOpen ? 'header__hamburger--active' : ''}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span class="header__hamburger-bar"></span>
+          <span class="header__hamburger-bar"></span>
+          <span class="header__hamburger-bar"></span>
+        </div>
+      </nav>
+    </header>
+  );
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
